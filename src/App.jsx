828857dd97415457eda0613af2ea5554a63c64f4
QcbjWUSTL <b.quinten@wustl.edu>
2026-03-29 17:35:08 -0500

    update handleClick to alternate between X and O

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,15 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
-    setSquares(nextSquares);
+  const nextSquares = squares.slice();
+  if (xIsNext) {
+    nextSquares[i] = "X";
+  } else {
+    nextSquares[i] = "O";
   }
+  setSquares(nextSquares);
+  setXIsNext(!xIsNext);
+}
 
   return (
     <>
